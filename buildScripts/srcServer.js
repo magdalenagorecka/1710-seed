8b418137c9e6eb9b4a5cbc588b51c822ab871783
Magdalena Górecka <goorecka.magdalena@gmail.com>
2017-10-07 22:25:24 +0200

    working server clean up

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -7,6 +7,4 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/src/index.html'));
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.listen(3000)
