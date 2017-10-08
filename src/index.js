c61c27bb31e23aef2e5ffc3be3dfd3d1f4fb7bfa
Magdalena Górecka <goorecka.magdalena@gmail.com>
2017-10-08 22:27:17 +0200

    sourcemapping

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
+
+import './index.css';
+
 import numeral from 'numeral';
 
+
 const courseValue = numeral(1000).format('$0,0.0');
+debugger;
 console.log(`I would pay ${courseValue} for this awesome course!`);
