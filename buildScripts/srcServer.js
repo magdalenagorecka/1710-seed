588f96ade447d2210c5484e7dac74c2ecf749ccf
Magdalena Górecka <goorecka.magdalena@gmail.com>
2017-10-08 08:05:43 +0200

    ES6 update

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,7 +1,7 @@
 
-const express = require('express')
+import express from 'express';
+import path from 'path';
 const app = express()
-const path = require('path')
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/src/index.html'));
