8014fb3e2210d098a1eb8352aeb7f938cd39dc1c
Magdalena Górecka <goorecka.magdalena@gmail.com>
2017-10-11 08:24:15 +0200

    Message update

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,5 @@
 import chalk from 'chalk';
 /* eslint-disable no-console */
-console.log(chalk.red.bgMagenta('Starting app in dev mode...'));
+console.log(chalk.black.bgMagenta('XOXOXOXOOXOXOXOXOOXXOXOXOX'));
+console.log(chalk.black.bgMagenta('Starting app in dev mode...'));
+console.log(chalk.black.bgMagenta('XOXOXOXOOXOXOXOXOXOXOXOXOX'));
