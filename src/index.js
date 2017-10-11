7c6e85115dfbaf4bedc0213746afb62d7268ecc8
Magdalena Górecka <goorecka.magdalena@gmail.com>
2017-10-11 21:10:24 +0200

    property name mistake

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ getUsers().then(result => {
         userBody += `<tr>
         <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
         <td>${user.id}</td>
-        <td>${user.name}</td>
+        <td>${user.firstName}</td>
         <td>${user.lastName}</td>
         <td>${user.email}</td>
         </tr>`        
